d924e674274e2a1bb4bd3a28d0d4f4a5b2da5bb4
Colin Rosenthal <csr@statsbiblioteket.dk>
2014-05-27 14:10:43 +0200

    Read for release.

---
+++ src/test/java/dk/statsbiblioteket/larm_doms_exporter/consumer/processors/DoExportProcessorTest.java
@@ -0,0 +1,13 @@
+package dk.statsbiblioteket.larm_doms_exporter.consumer.processors;
+
+import org.junit.Test;
+
+/**
+ * Created by csr on 5/27/14.
+ */
+public class DoExportProcessorTest {
+    @Test
+    public void testProcessThis() throws Exception {
+
+    }
+}
